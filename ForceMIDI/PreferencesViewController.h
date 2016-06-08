745229d9870514352ebb8cd057bff8dd1bdc8634
Joshua Breeden <jbreeden@me.com>
2016-06-07 23:30:14 -0400

    Add PreferencesViewController

---
+++ ForceMIDI/PreferencesViewController.h
@@ -0,0 +1,13 @@
+//
+//  PreferencesViewController.h
+//  ForceMIDI
+//
+//  Created by Joshua Breeden on 6/7/16.
+//  Copyright © 2016 Joshua Breeden. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface PreferencesViewController : NSViewController
+
+@end
---
+++ ForceMIDI/PreferencesViewController.m
@@ -0,0 +1,22 @@
+//
+//  PreferencesViewController.m
+//  ForceMIDI
+//
+//  Created by Joshua Breeden on 6/7/16.
+//  Copyright © 2016 Joshua Breeden. All rights reserved.
+//
+
+#import "PreferencesViewController.h"
+
+@interface PreferencesViewController ()
+
+@end
+
+@implementation PreferencesViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do view setup here.
+}
+
+@end
